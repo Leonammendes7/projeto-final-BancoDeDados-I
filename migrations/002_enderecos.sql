01edcbd0ead315391506dc0327a3c7480e85748a
melt44 <guilherme44pimenta@gmail.com>
2026-07-02 17:43:42 -0300

    add: sql utilizado para gerar tabela 'endereços' com 'id_usuario' como FK

---
+++ migrations/002_enderecos.sql
@@ -0,0 +1,17 @@
+CREATE TABLE public.enderecos (
+  id_endereco uuid NOT NULL PRIMARY KEY default gen_random_uuid (),
+  id_usuario uuid NOT NULL REFERENCES public.usuarios(id_usuario) ON DELETE CASCADE,
+  estado_sigla char(2),
+  estado varchar(30) NOT NULL,
+  cidade varchar(40) NOT NULL,
+  bairro varchar(40) NOT NULL,
+  complemento varchar(50),
+  numero varchar(20),
+  cep char(8) NOT NULL,
+  logradouro varchar(200) NOT NULL
+
+  -- verificações / constraints
+
+  CONSTRAINT validar_cep_numero CHECK (cep ~ '^\d{8}$') -- check pra ver se o cep tem apenas números
+  CONSTRAINT validar_estado_sigla CHECK (estado_sigla ~ '^[A-Z]{2}$') -- garante que esse campo contenha apenas 2 siglas maiúsculas
+)
